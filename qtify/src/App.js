00d59671d31cae8e089fda874b76b54db17cefd7
shraddha potekar <shraddhapotekar762@gmail.com>
2024-08-28 22:28:05 +0530

    section2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
-//import ColorTabs from "./components/Tab/Tab";
 function App() {
   return (
     <div className="App">
@@ -13,7 +12,6 @@ function App() {
       <Hero/> 
       <Section name={"Top Albums"} url={"https://qtify-backend-labs.crio.do/albums/top"}/> 
       <Section name={"New Albums"} url={"https://qtify-backend-labs.crio.do/albums/new"}/> 
-{/* <ColorTabs/> */}
       </>}></Route>
     </Routes>
     </BrowserRouter>
